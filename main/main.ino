e14f566aef5af44e03208e9552fd9a70396370f2
TOT102 <73033834+TOT102@users.noreply.github.com>
2024-12-19 17:12:55 +0200

    Create main.ino

---
+++ main/main.ino
@@ -0,0 +1,57 @@
+//*********** Driver pins ***********
+#define M1A 2 // M1 controls the elecctromagnets
+#define M1B 3
+#define M2A 4 // M2 controls the LED strip
+#define M2B 5
+//***********************************
+#define potPin A1
+
+const float minFrequency = 10; // min frequency in Hz
+const float maxFrequency = 12; // max frequency in Hz
+
+float oscFrequency = 0; // initial frequebcy 
+unsigned long oscPeriod = 0;
+
+unsigned long lastToggleTime = 0; 
+bool solenoidState = false; 
+bool ledState = false;  
+
+void setup(){
+  pinMode(M1A, OUTPUT);
+  pinMode(M1B, OUTPUT);
+  pinMode(M2A, OUTPUT);
+  pinMode(M2B, OUTPUT);
+
+  pinMode(potPin, INPUT);
+}
+
+void loop(){
+  int potVal = analogRead(potPin);
+  oscFrequency = map(potVal, 0, 1023, minFrequency * 10, maxFrequency * 10) / 10.0;
+
+  oscPeriod = 1000 / (oscFrequency*2) ;
+ 
+  unsigned long currentTime = millis();
+  if (currentTime - lastToggleTime >= oscPeriod) {
+    lastToggleTime = currentTime; 
+
+    solenoidState = !solenoidState;
+    if (solenoidState) {
+      digitalWrite(M1A, HIGH);
+      digitalWrite(M1B, LOW);
+    } else {
+      digitalWrite(M1A, LOW);
+      digitalWrite(M1B, HIGH);
+    }
+
+    ledState = !ledState;
+    if (ledState) {
+      digitalWrite(M2A, HIGH);
+      digitalWrite(M2B, LOW);
+    } else {
+      digitalWrite(M2A, LOW);
+      digitalWrite(M2B, HIGH);
+    }
+  }
+}
+
