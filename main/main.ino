18527ecc6c935677f856f2041c60c53940e710e6
TOT102 <73033834+TOT102@users.noreply.github.com>
2025-02-15 20:14:24 +0200

    Update main.ino
    Use micros() instead of hardware timers

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,8 @@
 #define DEF_OFFSET 50
-#define EXPO 300  // how long the LED is enabled in microseconds
-#define PERIOD 10000  // period of flickering in microseconds
-#define SET_TIMER 500
+#define EXPO 300  // LED on-time in microseconds
+#define PERIOD 5000  // Flickering period in microseconds
 #define ADC_PRESCALER 32
-#define FIXED_OSC_PERIOD 300000  
+#define FIXED_OSC_PERIOD 14000  
 
 #define M1A 5
 #define M1B 6
@@ -11,31 +10,15 @@
 #define M2B 10
 #define POT_PIN A0
 
+uint16_t setTimer, ledTimer, solenoidTimer;
 volatile bool solenoidState = false;
 volatile bool ledState = false;
 volatile uint16_t flashDelay = PERIOD * 2 + DEF_OFFSET;
 uint32_t lastPotRead = 0;
-
-void setADCprescaler(uint8_t prescaler) {
-  ADC0.CTRLA &= ~ADC_ENABLE_bm; // Disable ADC before changing prescaler
-  ADC0.CTRLC = (ADC0.CTRLC & ~ADC_PRESC_gm) | prescaler; 
-  ADC0.CTRLA |= ADC_ENABLE_bm; // Re-enable ADC 
-}
-
-uint16_t readADC() { 
-  ADC0.MUXPOS =  ADC_MUXPOS_AIN0_gc;      
-  ADC0.INTFLAGS = ADC_RESRDY_bm;  // Clear old flag
-  ADC0.COMMAND = ADC_STCONV_bm;   // Start ADC conversion
-  
-  while (!(ADC0.INTFLAGS & ADC_RESRDY_bm)) {}  
-
-  ADC0.INTFLAGS = ADC_RESRDY_bm;  // Clear flag
-  return ADC0.RES;                
-}
+uint32_t lastSolenoidToggle = 0;
+uint32_t lastLEDToggle = 0;
 
 void setup() {
-  cli(); // no interupts
-
   pinMode(M1A, OUTPUT);
   pinMode(M1B, OUTPUT);
   pinMode(M2A, OUTPUT);
@@ -46,69 +29,51 @@ void setup() {
   digitalWrite(M2A, LOW);
   digitalWrite(M2B, LOW);
 
-  setADCprescaler(ADC_PRESCALER); 
-
-  // Set up TCA0 (16-bit Timer) for solenoid control 
-  TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV64_gc | TCA_SINGLE_ENABLE_bm;  // Set prescaler to 64 and enable timer
-  TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_SINGLESLOPE_gc; // CTC mode 
-  TCA0.SINGLE.PER = (FIXED_OSC_PERIOD / 64) - 1; 
-  TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;
-
-  // Set up TCB0 (8-bit Timer) for LED control
-  TCB0.CTRLA = TCB_CLKSEL_CLKDIV2_gc | TCB_ENABLE_bm;
-  TCB0.CTRLB = TCB_CNTMODE_INT_gc;
-  TCB0.CCMP = EXPO / 64;
-  TCB0.INTCTRL = TCB_CAPT_bm;
-
-  Serial.begin(115200);
-
-  sei();
+  setADCrate(2);
 }
 
 void loop() {
-  if (millis() - lastPotRead >= SET_TIMER) {
-    lastPotRead = millis();
-    uint16_t potValue = analogRead(POT_PIN); 
-    float oscFrequency = map(potValue, 0, 1023, 1000, 6000) / 10.0;
-    Serial.println(oscFrequency);
-    noInterrupts();
-    flashDelay = 1000000 / (oscFrequency * 2);
-    TCB0.CCMP = EXPO / 64;  
-    interrupts();
+  uint32_t currentTime = micros();
+  
+  if (micros() - lastPotRead >= setTimer) {
+    lastPotRead = micros();
+    uint16_t potValue = analogRead(POT_PIN);
+    flashDelay = PERIOD * 2 + potValue - 512;
   }
-}
 
-ISR(TCA0_OVF_vect) {
-//**** SOLENOID CONTROL ****
-
-/*PORTB |= (1 << PB2);    // Turn ON pin 5  (M1A)
-  PORTD |= ~(1 << PD7);  // Turn OFF pin 6 (M1B)
-  PORTB &= ~(1 << PB2); // Turn OFF pin 5 (M1A)
-  PORTD |= (1 << PD7); // Turn ON pin 6  (M1B) */
-
-  solenoidState = !solenoidState;  
-  if (solenoidState) {
-    digitalWrite(M1A, HIGH);
-    digitalWrite(M1B, LOW);  
-  } else {
-    digitalWrite(M1A, LOW);
-    digitalWrite(M1B, HIGH);  
-  }
-  TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm;  // Clear the interrupt flag
-}
-
-ISR(TCB0_INT_vect) {
-//**** LED CONTROL ****
-  static bool flashState = false;
-  flashState = !flashState;
-  if (flashState) {
+  // LED Control
+  if (currentTime - lastLEDToggle >= (ledState ? EXPO : (flashDelay - EXPO))) {
+    lastLEDToggle = currentTime;
+    ledState = !ledState;
     digitalWrite(M2A, LOW);
-    digitalWrite(M2B, LOW);
-    TCB0.CCMP = EXPO / 64;
-  } else {
-    digitalWrite(M2A, LOW);
-    digitalWrite(M2B, HIGH);
-    TCB0.CCMP = (flashDelay - EXPO) / 64;
+    digitalWrite(M2B, ledState ? HIGH : LOW);
   }
-  TCB0.INTFLAGS = TCB_CAPT_bm;  // Clear the interrupt flag
+
+  // Solenoid Control
+  if (currentTime - lastSolenoidToggle >= FIXED_OSC_PERIOD) {
+    lastSolenoidToggle = currentTime;
+    solenoidState = !solenoidState;
+    digitalWrite(M1A, solenoidState);
+    digitalWrite(M1B, !solenoidState);
+  }
+
+}
+void setADCrate(uint8_t mode) {
+  // Disable ADC before changing settings
+  ADC0.CTRLA &= ~ADC_ENABLE_bm;
+
+  // Set prescaler based on mode
+  switch (mode) {
+    case 0:  ADC0.CTRLC = ADC_PRESC_DIV2_gc;   break; // 8 MHz
+    case 1:  ADC0.CTRLC = ADC_PRESC_DIV4_gc;   break; // 4 MHz
+    case 2:  ADC0.CTRLC = ADC_PRESC_DIV8_gc;   break; // 2 MHz
+    case 3:  ADC0.CTRLC = ADC_PRESC_DIV16_gc;  break; // 1 MHz (safe)
+    case 4:  ADC0.CTRLC = ADC_PRESC_DIV32_gc;  break; // 500 kHz
+    case 5:  ADC0.CTRLC = ADC_PRESC_DIV64_gc;  break; // 250 kHz (default)
+    case 6:  ADC0.CTRLC = ADC_PRESC_DIV128_gc; break; // 125 kHz
+    default: ADC0.CTRLC = ADC_PRESC_DIV64_gc;  break; // Default to safe mode
+  }
+
+  // Re-enable ADC
+  ADC0.CTRLA |= ADC_ENABLE_bm;
 }
